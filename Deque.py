b2d9c65a0f5765f54c9fcfdc180a7023621ec92e
sami10644 <abdullahassami@gmail.com>
2021-08-10 22:09:56 +0600

    my fifth commit

---
+++ Deque.py
@@ -0,0 +1,15 @@
+
+from collections import deque
+
+customQueue = deque(maxlen = 3)
+print(customQueue)
+
+customQueue.append(2)
+customQueue.append(4)
+customQueue.append(6)
+customQueue.append(9) #Overrides 1st element if maxlen is specified
+print(customQueue)
+print(customQueue.popleft())
+print(customQueue)
+print(customQueue.clear())
+print(customQueue)
